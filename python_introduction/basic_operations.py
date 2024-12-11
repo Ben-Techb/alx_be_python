d7879eab54158447f6419811d6464a96fd368711
root <benbazirake9@gmail.com>
2024-12-11 04:00:25 -0800

    add the new file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+addition = number1 + number2
+Subtraction= number1 - number2
+Multiplication = number1 * number2
+print(f"the addition of 10 and  5 is {addition}")
+print(f"the Subtraction of 10 and  5 is {Subtraction}")
+print(f"the Multiplication of 10 and  5 is  {Multiplication}")
