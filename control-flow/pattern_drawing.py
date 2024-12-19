3e2293cbd800a1672c4501141a585f6e7decb1da
root <benbazirake9@gmail.com>
2024-12-19 07:09:51 -0800

    add the change

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,7 +3,7 @@ if number <= 0:
     print("please enter the positive number ")
 else:
     row = 0
-    while row < number :
+while row < number :
         for _ in range(number):
             print("*" ,end="")
         print()
