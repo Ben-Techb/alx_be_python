9f33a324d49c780f475fe463fc17188282a0eeed
root <benbazirake9@gmail.com>
2025-01-08 01:41:49 -0800

    add the change to the file pleae

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")  # Ensure this line is present
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -46,3 +46,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
