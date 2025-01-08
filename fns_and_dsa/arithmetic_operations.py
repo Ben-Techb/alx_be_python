9f968843cfcbb372c662ce8531f57cb995aa20f3
root <benbazirake9@gmail.com>
2025-01-08 00:27:59 -0800

    add the change tot the file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,22 @@
-def perform_operation(num1,num2,operation):
-    if operation =="add":
+def perform_operation(num1, num2, operation):
+    """
+    Perform a basic arithmetic operation based on the input parameters.
+    
+    :param num1: First number (float)
+    :param num2: Second number (float)
+    :param operation: Operation to perform (string: 'add', 'subtract', 'multiply', or 'divide')
+    :return: Result of the operation or an appropriate error message
+    """
+    if operation == 'add':
         return num1 + num2
-    elif operation == "subtract":
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == "multiply":
-        return num1*num2
-    elif operation == "divide":
-         if num2 == 0:
-          return "the number is error it can be divided by zero"
-         return num1 / num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
     else:
-       return "invalide operation "
-           
+        return "Error: Invalid operation"
+
