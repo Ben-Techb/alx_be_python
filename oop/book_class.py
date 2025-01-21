0565ab335475ec23e35fe3f92d2b784919644d3c
root <benbazirake9@gmail.com>
2025-01-21 01:36:53 -0800

    add the change to the file

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,11 +1,19 @@
 class Book:
-    def __init__(self,title :str, author: str, year: int):
-        self.title= title
-        self.author= author
+    def __init__(self, title, author, year):
+        """Initialize a Book instance."""
+        self.title = title
+        self.author = author
         self.year = year
+
     def __del__(self):
-        return f"Deleting {self.title}"
+        """Destructor to print a message when the object is deleted."""
+        print(f"Deleting '{self.title}'")
+
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.title}"
+        """String representation of the Book."""
+        return f"'{self.title}' by {self.author}, published in {self.year}"
+
     def __repr__(self):
-        return f"Book('{self.title}','{self.author}',{self.year})
+        """Official representation of the Book."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
